fe132dbf219a363546a9467041f870cf9aa683b3
Ishika Agarwal <boilermakerishika@gmail.com>
2026-02-23 21:42:52 -0600

    add readme

---
+++ nisvals_list_for_global_piqa.py
@@ -0,0 +1,18 @@
+global_piqa_subsets_dict = {
+    "cmn_hans": "Chinese",
+    "eng_latn": "English",
+    "fra_latn_fran": "French",
+    "spa_latn_mexi": "Spanish",
+    "por_latn_braz": "Portuguese",
+    "deu_latn": "German",
+    "ita_latn": "Italian",
+    "rus_cyrl": "Russian",
+    "jpn_jpan": "Japanese",
+    "kor_hang": "Korean",
+    "vie_latn": "Vietnamese",
+    "tha_thai": "Thai",
+    "arb_arab": "Arabic",
+    "ben_beng": "Hindi",
+    "tur_latn": "Turkish",
+    "hin_deva": "Bengali"
+}
